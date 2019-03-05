8134f726f449d1f83c816e66a3c7d340f62e23e5
Michael P. Moran <mmutexas@gmail.com>
2019-03-05 11:45:42 -0600

    add expected output for problems; fix logic for some problems; fix print formatting for square/cube problem

--- 4.3_control_structures_exercises.py
+++ 4.3_control_structures_exercises.py
@@ -63,8 +63,17 @@ while i <= 15:
 print()
 
 # Your output should look like this:
-
-# 5 6 7 8 9 10 11 12 13 14 15
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
 
 # Create a while loop that will count by 2's starting with 0 and ending at
 # 100. Follow each number with a new line.
@@ -85,6 +94,11 @@ print()
 
 # Create a while loop that starts at 2, and displays the number squared
 # on each line while the number is less than 1,000,000. Output should equal:
+#  2
+#  4
+#  16
+#  256
+#  65536
 i = 2
 while i < 1_000_000:
     print(i)
@@ -95,6 +109,26 @@ print()
 # 2 4 16 256 65536
 
 # Write a loop that uses print to create the output shown below.
+# 100
+# 95
+# 90
+# 85
+# 80
+# 75
+# 70
+# 65
+# 60
+# 55
+# 50
+# 45
+# 40
+# 35
+# 30
+# 25
+# 20
+# 15
+# 10
+# 5
 i = 100
 
 while i > 0:
@@ -107,6 +141,17 @@ print()
 
 # i. Write some code that prompts the user for a number, then shows a
 # multiplication table up through 10 for that number.
+# For example, if the user enters 7, your program should output:
+# 7 x 1 = 7
+# 7 x 2 = 14
+# 7 x 3 = 21
+# 7 x 4 = 28
+# 7 x 5 = 35
+# 7 x 6 = 42
+# 7 x 7 = 49
+# 7 x 8 = 56
+# 7 x 9 = 63
+# 7 x 10 = 70
 user_num = int(input("Enter a number: "))
 for i in range(1, 11):
     print(f"{user_num} x {i} = {user_num * i}")
@@ -114,6 +159,15 @@ for i in range(1, 11):
 print()
 
 # ii. Create a for loop that uses print to create the output shown below.
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+# 88888888
+# 999999999
 for i in range(1, 10):
     print(str(i) * i)
 
@@ -126,8 +180,41 @@ print()
 # (Hint: use the isdigit method on strings to determine this).
 # Use a loop and the continue statement to output all the odd numbers
 # between 1 and 50, except for the number the user entered.
+# Your output should look like this:
+# Number to skip is: 27
+
+# Here is an odd number: 1
+# Here is an odd number: 3
+# Here is an odd number: 5
+# Here is an odd number: 7
+# Here is an odd number: 9
+# Here is an odd number: 11
+# Here is an odd number: 13
+# Here is an odd number: 15
+# Here is an odd number: 17
+# Here is an odd number: 19
+# Here is an odd number: 21
+# Here is an odd number: 23
+# Here is an odd number: 25
+# Yikes! Skipping number: 27
+# Here is an odd number: 29
+# Here is an odd number: 31
+# Here is an odd number: 33
+# Here is an odd number: 35
+# Here is an odd number: 37
+# Here is an odd number: 39
+# Here is an odd number: 41
+# Here is an odd number: 43
+# Here is an odd number: 45
+# Here is an odd number: 47
+# Here is an odd number: 49
+
 user_num = input("Enter an odd number between 1 and 50: ")
-while not user_num.isdigit() or int(user_num) % 2 == 0:
+while (
+    not user_num.isdigit()
+    or int(user_num) % 2 == 0
+    or not int(user_num) in range(1, 51)
+):
     user_num = input("Enter an odd number between 1 and 50: ")
 print()
 print("Number to skip is:", int(user_num))
@@ -176,13 +263,15 @@ print()
 for i in range(1, 101):
     # For numbers which are multiples of both three and five print "FizzBuzz"
     if i % 5 == 0 and i % 3 == 0:
-        print(i, "FizzBuzz")
+        print("FizzBuzz")
     # For multiples of three print "Fizz" instead of the number
     elif i % 3 == 0:
-        print(i, "Fizz")
+        print("Fizz")
     # For the multiples of five print "Buzz".
     elif i % 5 == 0:
-        print(i, "Buzz")
+        print("Buzz")
+    else:
+        print(i)
 
 print()
 
@@ -198,12 +287,26 @@ while wants_to_continue.lower() == "y" or wants_to_continue.lower() == "yes":
     print("------ | ------- | -----")
     # Display a table of squares and cubes from 1 to the value entered.
     for i in range(1, int(user_num) + 1):
-        print(f"{i: 6} | {i ** 2: 7} | {i ** 3: 5}")
+        print(f"{i: <6} | {i ** 2: <7} | {i ** 3: <5}")
 
     # Ask if the user wants to continue.
     wants_to_continue = input("\nWould you like to continue? ")
     # Assume that the user will enter valid data.
     # Only continue if the user agrees to.
+
+# Example Output
+
+# What number would you like to go up to? 5
+
+# Here is your table!
+
+# number | squared | cubed
+# ------ | ------- | -----
+# 1      | 1       | 1
+# 2      | 4       | 8
+# 3      | 9       | 27
+# 4      | 16      | 64
+# 5      | 25      | 125
 
 print()
 
